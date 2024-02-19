8017a098c6b61fe4a08b97c3b46c7704b79f97d8
louis <luis.mates@universo.univates.br>
2024-02-19 16:15:01 -0300

    criada tela para seleção de dificuldade e setando ela como tela inicial

---
+++ CampoMinado/Tamanho.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CampoMinado
+{
+    public partial class Tamanho : Window
+    {
+        public Tamanho() 
+        {
+            this.InitializeComponent();
+        }
+    }
+}
